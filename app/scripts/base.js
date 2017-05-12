57cae5991301853e8a0de54d4c88c6c058cda6db
Jamie Waxberg <waxberg@cleverbridge.com>
2017-05-11 20:15:51 -0500

    adding screenshot modals

--- app/scripts/base.js
+++ app/scripts/base.js
@@ -1,1 +1,13 @@
 'use strict';
+ 
+
+$(document).ready(function () {
+	$('.see-more').on('click', function() {
+		$('#overlay').fadeIn();
+		$(this).next('.modal').fadeIn();
+	});
+	$('#close').on('click', function() {
+		$('#overlay').hide();
+		$('.modal').hide();
+	});
+});
